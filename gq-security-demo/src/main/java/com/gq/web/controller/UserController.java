e10e7072b80d72ae8eb775cccdd041bad4229da3
youxuehu <youxuehu@hotmail.com>
2018-10-20 21:12:01 +0800

    提交

--- gq-security-demo/src/main/java/com/gq/web/controller/UserController.java
+++ gq-security-demo/src/main/java/com/gq/web/controller/UserController.java
@@ -4,6 +4,9 @@ package com.gq.web.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,11 @@ import com.gq.dto.User;
 
 @RestController
 public class UserController {
+
+	@GetMapping("/me")
+	public Object getCurrentUser(Authentication authentication){
+		return authentication;
+	}
 
 	@RequestMapping(value="/user",method=RequestMethod.GET)
 	public List<User> query(String userName){
