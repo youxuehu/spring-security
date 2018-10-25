35420ceb5ac8ea1457a15c1e19243ee9c0ac6db5
yxh <15600550173@163.com>
2018-10-25 16:51:56 +0800

    图形验证码重构   配置

--- gq-security-demo/src/main/java/com/gq/DemoApplication.java
+++ gq-security-demo/src/main/java/com/gq/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.gq;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +15,13 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
+
+	@Autowired private RedisTemplate redisTemplate;
 	@GetMapping("/hello")
 	public String hello() {
+		System.out.println(redisTemplate);
+		SetOperations set = redisTemplate.opsForSet();
+		set.add("name","jack");
 		return "hello spring security";
 	}
 }
